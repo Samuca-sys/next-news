e2fca8b67fbf2a9128b3973f26246ac954946f02
Samuel Macedo <samuel.macfer@gmail.com>
2021-05-24 21:29:22 -0300

    ci: add db.json typicode link

--- services/api.js
+++ services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const employeesApi = axios.create({
-	baseURL: 'http://localhost:3333/',
+	baseURL:
+		'https://my-json-server.typicode.com/samuca-sys/next-news' ||
+		'http://localhost:3333/',
 });
 
 export const newsApi = axios.create({
